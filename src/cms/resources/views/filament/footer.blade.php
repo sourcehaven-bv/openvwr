58c6af6f9d82d1f0f1b54e5b1a0aae1a48a2331b
Jeroen Vloothuis <jvloothuis@sourcehaven.nl>
2026-07-24 17:08:09 +0200

    feat: show deployment version in footer + meta tag

---
+++ src/cms/resources/views/filament/footer.blade.php
@@ -0,0 +1,9 @@
+<div class="fi-footer flex justify-center px-6 py-4 text-xs text-gray-500 dark:text-gray-400">
+    <span>
+        <a href="https://openvwr.nl" target="_blank" rel="noopener noreferrer" class="hover:underline">OpenVWR</a>
+        by
+        <a href="https://sourcehaven.nl" target="_blank" rel="noopener noreferrer" class="hover:underline">Sourcehaven B.V.</a>
+        <span class="mx-1">&middot;</span>
+        {{ config('version.label') }}
+    </span>
+</div>
