f9b7a4e77ab9dc6a5d805d8e8aacd49727b1875a
Jeroen Vloothuis <jvloothuis@sourcehaven.nl>
2026-08-27 12:45:16 +0200

    feat(labels): toon aantal gekoppelde items

--- src/cms/resources/lang/en/tag.php
+++ src/cms/resources/lang/en/tag.php
@@ -10,6 +10,7 @@ return [
     'create_placeholder' => 'Add labels',
     'hint_icon_text' => 'Labels are a powerful, dynamic way to tag processing activities. The labels are useful for reporting because they make it possible to identify processing activities relating to a specific function or property. Anyone can create labels and they are shared across processing activities.',
 
+    'items_count' => 'Linked items',
     'name' => 'Name',
     'type' => 'Type',
 ];
