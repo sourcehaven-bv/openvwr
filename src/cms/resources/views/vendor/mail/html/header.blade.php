20781e20f9a473f9cce516c8828e81f8da084d8c
Jeroen Vloothuis <jvloothuis@sourcehaven.nl>
2026-09-01 22:59:24 +0200

    fix: escape slot-output in mailheader

--- src/cms/resources/views/vendor/mail/html/header.blade.php
+++ src/cms/resources/views/vendor/mail/html/header.blade.php
@@ -3,12 +3,16 @@
     are blocked by default in most clients, which would leave the header
     empty. A styled wordmark on a coral rule always renders, so every login
     mail is recognisably ours even with images turned off.
+
+    The slot is escaped, unlike the Laravel template this is based on: it only
+    ever carries config('app.name'), so there is no markup to preserve and no
+    reason to opt out of escaping by default.
 --}}
 @props(['url'])
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<span class="brand-name">{!! $slot !!}</span>
+<span class="brand-name">{{ $slot }}</span>
 </a>
 </td>
 </tr>
